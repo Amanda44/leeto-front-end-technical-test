e862185ce862d64c7db9b69d0a18b6d2cb933d81
Amanda44 <amanda.mellerin@gmail.com>
2025-03-04 14:19:44 +0100

    refacto: Manage errors on data queries

--- app/src/hooks/GiftCardsList/ActiveGiftCards/query.tsx
+++ app/src/hooks/GiftCardsList/ActiveGiftCards/query.tsx
@@ -1,11 +1,31 @@
 import { GiftCard } from "../../../types/GiftCard";
 
 export const getActiveGiftCards = async (): Promise<GiftCard[]> => {
-  /* TODO: better managing of errors */
-  const response = await fetch(`http://localhost:3001/gift-cards?state=active`);
-  const data = await response.json();
-  if (response.status !== 200) {
-    throw data;
+  try {
+    const response = await fetch(
+      `http://localhost:3001/gift-cards?state=active`
+    );
+    const data = await response.json();
+    if (response.status !== 200 || !response.ok) {
+      console.error(
+        "Erreur: Impossible de récupérer les cartes cadeaux actives"
+      );
+      throw new Error(
+        "Erreur: Impossible de récupérer les cartes cadeaux actives"
+      );
+    }
+    return data;
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error("Erreur: " + e.message);
+      throw new Error("Erreur: " + e.message);
+    } else {
+      console.error(
+        "Erreur: Impossible de récupérer les cartes cadeaux actives"
+      );
+      throw new Error(
+        "Erreur: Impossible de récupérer les cartes cadeaux actives"
+      );
+    }
   }
-  return data;
 };
--- app/src/hooks/GiftCardsList/ArchivedGiftcards/query.tsx
+++ app/src/hooks/GiftCardsList/ArchivedGiftcards/query.tsx
@@ -1,13 +1,31 @@
 import { GiftCard } from "../../../types/GiftCard";
 
 export const getArchivedGiftCards = async (): Promise<GiftCard[]> => {
-  /* TODO: better managing of errors */
-  const response = await fetch(
-    `http://localhost:3001/gift-cards?state=archived`
-  );
-  const data = await response.json();
-  if (response.status !== 200) {
-    throw data;
+  try {
+    const response = await fetch(
+      `http://localhost:3001/gift-cards?state=archived`
+    );
+    const data = await response.json();
+    if (response.status !== 200 || !response.ok) {
+      console.error(
+        "Erreur: Impossible de récupérer les cartes cadeaux clôturées"
+      );
+      throw new Error(
+        "Erreur: Impossible de récupérer les cartes cadeaux clôturées"
+      );
+    }
+    return data;
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error("Erreur: " + e.message);
+      throw new Error("Erreur: " + e.message);
+    } else {
+      console.error(
+        "Erreur: Impossible de récupérer les cartes cadeaux clôturées"
+      );
+      throw new Error(
+        "Erreur: Impossible de récupérer les cartes cadeaux clôturées"
+      );
+    }
   }
-  return data;
 };
